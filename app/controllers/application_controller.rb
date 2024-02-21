ea24ec5c72644b4c35a8e84787990ca174da57bd
Daniel Londoño <daniel.esloh@gmail.com>
2024-02-21 17:23:19 -0500

    The concern json_web_token was created and configured in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include JsonWebToken
+  before_action :autenticate_request
+
+  private
+  
+  def autenticate_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
+  end
 end
