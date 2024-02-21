8ab10e762e32d7892bb452342a97ae28cebf3350
Daniel Londoño <daniel.esloh@gmail.com>
2024-02-21 18:06:28 -0500

    Correcting authentication_controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,9 +1,11 @@
 class AuthenticationController < ApplicationController
+  skip_before_action :autenticate_request, only: %i[login]
+
   # POST /auth/login
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token = JsonWebToken.jwt_encode(user_id: @user.id)
+      token = jwt_encode(user_id: @user.id)
       render json: { token: token }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
